36abe8a0a2c0a9ec2c23987fadbf36beedea7d73
Wes Schlichter <wschlichter@gopivotal.com>
2013-09-19 12:51:44 -0500

    Fixed issues with vfabric repo and tcserver group creation

--- puppet/vfabric_repo/manifests/init.pp
+++ puppet/vfabric_repo/manifests/init.pp
@@ -12,12 +12,12 @@
 
 
 class vfabric_repo ( 
-  $all = "false",
+  $all = false,
   $ensure = "installed",
   $i_accept_eula = false
   ) {
   
-  $vfabric5_repo_url = "http://repo.vmware.com/pub/rhel5/vfabric/5/vfabric-5-repo-5-2.noarch.rpm"
+  $vfabric5_repo_url = "http://repo.vmware.com/pub/rhel5/vfabric/5.3/vfabric-5.3-repo-5.3-1.noarch.rpm"
   $vfabric_all_repo_url = "http://repo.vmware.com/pub/rhel5/vfabric-all/vfabric-all-repo-1-1.noarch.rpm"
   
   if $osfamily == "RedHat" and $i_accept_eula == true{
@@ -25,31 +25,22 @@ class vfabric_repo (
       provider => 'rpm',
       ensure => $ensure,
       source => $vfabric5_repo_url,
-      require => File['/etc/vmware/vfabric/accept-vfabric-eula.txt']
+      before => Exec['vfabric-eula-acceptance']
     }
 
-    if $all == "true" {
+    if $all {
       package { 'vfabric-all-repo':
         provider => 'rpm',
         ensure => $ensure,
-        source => $vfabric_all_url
+        source => $vfabric_all_repo_url
       }
     }
-    
-    file {"/etc/vmware/vfabric/accept-vfabric-eula.txt":
-      content => "I_ACCEPT_EULA_LOCATED_AT=http://www.vmware.com/download/eula/vfabric_app-platform_eula.html",
-      ensure => present,
+   
+    exec {'vfabric-eula-acceptance':
+        command => '/etc/vmware/vfabric/vfabric-5.3-eula-acceptance.sh --accept_eula_file=VMware_EULA_20120515b_English.txt > /dev/null 2>&1',
+        require => Package['vfabric-5-repo']
     }
-
-    file {"/etc/vmware":
-      ensure => directory
-    }
-
-    file {"/etc/vmware/vfabric":
-      ensure => directory,
-      require => File['/etc/vmware']
-    }
-                                  
+   
   } else {
       alert "[${fqdn}] You must accept the terms of the EULA located at http://www.vmware.com/download/eula/vfabric_app-platform_eula.html"
   }
