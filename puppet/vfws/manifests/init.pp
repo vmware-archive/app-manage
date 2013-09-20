beb8b913755f1aa436d6aa9094eb64aa68d8e040
Wes Schlichter <wschlichter@gopivotal.com>
2013-09-20 10:30:09 -0500

    Added .rpm usage to vfws module, fixed an incorrect reference to vfabric repo

--- puppet/vfws/manifests/init.pp
+++ puppet/vfws/manifests/init.pp
@@ -10,9 +10,11 @@
 
 class vfws ( $vfws_version = "5.3.1-2-x86_64-linux-glibc2",
   $vfws_user = "vfws",
-  $install_path = "/opt/vmware" 
+  $install_path = "/opt/vmware",
+  $uses_templates = false,
+  $template_dir = "templates",
+  $templates_source = "puppet:///modules/vfws/templates"  #Location to copy templates from
   ) {
-
 
   $module = "vfws"
   $prefix = "/etc/puppet/modules"
@@ -23,23 +25,44 @@ class vfws ( $vfws_version = "5.3.1-2-x86_64-linux-glibc2",
   $installed_base = "${install_path}/${basename}"
   Exec { path => "/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin" }
 
-  file {
-    "/var/tmp/${installer}":
-      source  => [ "${p1}/${installer}", "${p2}/${installer}" ],
-  }
-  file { "${install_path}":
+  if defined('vfabric_repo') {
+    package {'vfabric-web-server':
+      provider => 'yum',
+      ensure => "installed",
+      require => [ Package['vfabric-5.3-repo'], Exec['vfabric-eula-acceptance'] ],
+    }
+    if $uses_templates {
+      file { "${installed_base}/${templates_dir}":
+        recurse => true,
+        source => "${templates_source}",
+        require => Package['vfabric-web-server']
+      }
+    }
+    file { "${installed_base}":
+      require => Package['vfabric-web-server']  #this just gives us something to require from the instance
+    }
+  } else {
+    file {
+      "/var/tmp/${installer}":
+        source  => [ "${p1}/${installer}", "${p2}/${installer}" ],
+    }
+    file { "${install_path}":
+        ensure => "directory",
+    }
+    exec { "unpack-vfws":
+        cwd => "${install_path}",
+        command => "/var/tmp/${installer}",
+        creates => "${installed_base}",
+        require => File["/var/tmp/${installer}"];
+    }
+    file { "${installed_base}":
       ensure => "directory",
+      require => Exec['unpack-vfws']
+    }
+    file { "${installed_base}/${templates_dir}":
+      recurse => true,
+      source => "${templates_source}",
+      require => Exec['unpack-vfws']
+    }
   }
-  exec { "unpack-vfws":
-      cwd => "${install_path}",
-      command => "/var/tmp/${installer}",
-      creates => "${installed_base}",
-      require => File["/var/tmp/${installer}"];
-  }
-  file { "${installed_base}":
-    ensure => "directory",
-    require => Exec['unpack-vfws']
-  }
-
 }
-
