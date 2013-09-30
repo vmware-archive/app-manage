284d7009cf12eb39a5e6e06808231f9df55c93b3
Wes Schlichter <wschlichter@gopivotal.com>
2013-09-30 13:50:57 -0500

    Removed manual install option

--- puppet/vfws/manifests/init.pp
+++ puppet/vfws/manifests/init.pp
@@ -17,11 +17,6 @@ class vfws ( $vfws_version = "5.3.1-2-x86_64-linux-glibc2",
   ) {
 
   $module = "vfws"
-  $prefix = "/etc/puppet/modules"
-  $p1 = "${prefix}/${module}/files"
-  $p2 = "puppet:///modules/${module}"
-  $basename = "vfabric-web-server"
-  $installer = "${basename}-${vfws_version}.zip.sfx"
   $installed_base = "${install_path}/${basename}"
   Exec { path => "/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin" }
 
@@ -42,27 +37,6 @@ class vfws ( $vfws_version = "5.3.1-2-x86_64-linux-glibc2",
       require => Package['vfabric-web-server']  #this just gives us something to require from the instance
     }
   } else {
-    file {
-      "/var/tmp/${installer}":
-        source  => [ "${p1}/${installer}", "${p2}/${installer}" ],
-    }
-    file { "${install_path}":
-        ensure => "directory",
-    }
-    exec { "unpack-vfws":
-        cwd => "${install_path}",
-        command => "/var/tmp/${installer}",
-        creates => "${installed_base}",
-        require => File["/var/tmp/${installer}"];
-    }
-    file { "${installed_base}":
-      ensure => "directory",
-      require => Exec['unpack-vfws']
-    }
-    file { "${installed_base}/${templates_dir}":
-      recurse => true,
-      source => "${templates_source}",
-      require => Exec['unpack-vfws']
-    }
+    fail "vfabric_repo module missing"
   }
 }
