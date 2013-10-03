7f3e3a9c0a4b6545a63bd2747cd3e9404848e6dc
Wes Schlichter <wschlichter@gopivotal.com>
2013-10-03 10:30:54 -0500

    Cleaned of formatting of code

--- puppet/vfabric_repo/manifests/init.pp
+++ puppet/vfabric_repo/manifests/init.pp
@@ -7,42 +7,47 @@
 ## http://www.vmware.com/download/eula/vfabric_app-platform_eula.html
 ##
 ## to accept the EULA the variable "i_accept_eula" must be set to true
-## 
+##
 ##
 
-
-class vfabric_repo ( 
+class vfabric_repo (
   $all = false,
-  $ensure = "installed",
+  $ensure = installed,
   $i_accept_eula = false
   ) {
-  
-  $vfabric5_repo_url = "http://repo.vmware.com/pub/rhel5/vfabric/5.3/vfabric-5.3-repo-5.3-1.noarch.rpm"
-  $vfabric_all_repo_url = "http://repo.vmware.com/pub/rhel5/vfabric-all/vfabric-all-repo-1-1.noarch.rpm"
-  
-  if $osfamily == "RedHat" and $i_accept_eula == true{
+  $vfabric5_repo_url = 'http://repo.vmware.com/pub/rhel5/vfabric/5.3/vfabric-5.3-repo-5.3-1.noarch.rpm'
+  $vfabric_all_repo_url = 'http://repo.vmware.com/pub/rhel5/vfabric-all/vfabric-all-repo-1-1.noarch.rpm'
+
+  if $::osfamily == 'RedHat' and $i_accept_eula {
     package { 'vfabric-5.3-repo':
+      ensure   => $ensure,
       provider => 'rpm',
-      ensure => $ensure,
-      source => $vfabric5_repo_url,
-      before => Exec['vfabric-eula-acceptance']
+      source   => $vfabric5_repo_url,
+      before   => Exec['vfabric-eula-acceptance']
     }
 
     if $all {
       package { 'vfabric-all-repo':
+        ensure   => $ensure,
         provider => 'rpm',
-        ensure => $ensure,
-        source => $vfabric_all_repo_url
+        source   => $vfabric_all_repo_url
       }
     }
-   
+
     exec {'vfabric-eula-acceptance':
         command => '/etc/vmware/vfabric/vfabric-5.3-eula-acceptance.sh --accept_eula_file=VMware_EULA_20120515b_English.txt > /dev/null 2>&1',
         require => Package['vfabric-5.3-repo']
     }
-   
+  } elsif $::osfamily == 'Debian' and $i_accept_eula == true {
+    if $::operatingsystemrelease == '10.04' {
+
+    } elsif $::operatingsystemrelease = '12.04' {
+
+    } else {
+      fail "Operating System Release ${::operatingsystemrelease} not supported"
+    }
   } else {
-      alert "[${fqdn}] You must accept the terms of the EULA located at http://www.vmware.com/download/eula/vfabric_app-platform_eula.html"
+      alert "[${::fqdn}] You must accept the terms of the EULA located at http://www.vmware.com/download/eula/vfabric_app-platform_eula.html"
   }
 }
 
