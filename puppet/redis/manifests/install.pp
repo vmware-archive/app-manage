6fc6bee34c686d382bbf5564be43ac62992ff22d
Wes Schlichter <wschlichter@gopivotal.com>
2013-10-08 10:17:58 -0500

    Reformatted code to pass rake lint tests

--- puppet/redis/manifests/install.pp
+++ puppet/redis/manifests/install.pp
@@ -1,9 +1,9 @@
+## Class redis::install
+#
+# See manifests/init.pp for variable definitions
 
-
-class redis::install ( $version = "latest" ) {
-  
+class redis::install ( $version = 'latest' ) {
   package {'redis':
     ensure => $version
   }
-
 }
