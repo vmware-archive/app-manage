f22ccd8e023d1b22699b87e9587549c943a8b920
Wes Schlichter <wschlichter@gopivotal.com>
2013-10-09 15:28:14 -0500

    Updated headers with license and configuration information

--- puppet/tcserver/manifests/init.pp
+++ puppet/tcserver/manifests/init.pp
@@ -1,14 +1,47 @@
-## Module: tcserver
+## Web Server Puppet Module
 ##
-## Installs tc Server
+## Copyright 2013 GoPivotal, Inc
 ##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
 ##
-## LEGAL NOTICE
+## http://www.apache.org/licenses/LICENSE-2.0
 ##
-## Use of this module and packages installed via this module require
-## acceptace of of the VMWare End User License Agreement located at
-## http://www.vmware.com/download/eula/vfabric_app-platform_eula.html
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
 ##
+## Configuration Information
+##
+##  $version
+##    Default - latest
+##    Specifies the version of the tc-server package to install. When value is
+##    latest then the latest version of the package available in repositories 
+##    will be used
+##
+##  $uses_templates
+##    Default - true
+##    Whether this installation should copy templates located in $template_source
+##    Instances should still have the template option used to specify the desired
+##    template
+##
+##  $templates_source
+##    Default - puppet:///modules/tcserver/templates
+##    The location of the template files to copy to the node. If the location is empty
+##    then no files are copied. Must be a valid puppet url
+##
+##  $tcserver_user
+##    Default - tcserver
+##    The user to user for ownership of files. This variable should be left at
+##    except for expert usage.
+##
+##  $tcserver_group
+##    Default - vfabric
+##    The user to user for ownership of files. This variable should be left at
+##    except for expert usage.
 
 class tcserver (
   $version = 'latest',
