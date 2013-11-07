713975937814480b4efe17bd399c600558743ac8
Wes Schlichter <wschlichter@gopivotal.com>
2013-11-07 14:11:53 -0600

    Made created-instance dependent on postinstall

--- puppet/pivotal-tcserver/manifests/instance.pp
+++ puppet/pivotal-tcserver/manifests/instance.pp
@@ -135,7 +135,7 @@ define tcserver::instance (
       cwd         => $cwd,
       command     => "${::tcserver::installed_base}/tcruntime-instance.sh create ${name} ${template_option} ${java_home_option} ${properties} ${properties_file_option} ${layout_option} ${version_option}",
       creates     => "${cwd}/${name}",
-      require     => Package['vfabric-tc-server-standard']
+      require     => Class['::tcserver::postinstall']
     }
 
     file { "${cwd}/${name}":
