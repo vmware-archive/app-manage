c38c92f58f3dd0dbde8d6b71348e97ee1eaa8314
Wes Schlichter <wschlichter@gopivotal.com>
2013-11-14 14:20:11 -0600

    Added missing param

--- puppet/pivotal-tcserver/lib/puppet/provider/tcruntime_instance/tcruntime_instance.rb
+++ puppet/pivotal-tcserver/lib/puppet/provider/tcruntime_instance/tcruntime_instance.rb
@@ -17,7 +17,6 @@ Puppet::Type.type(:tcruntime_instance).provide(:tcruntime_instance) do
   end
 
   def create
-    
     # We create the command here so we can get the java_home variable from the manifest
     resource = @resource
     Puppet::Provider.has_command(:tcruntime_instance, "/opt/vmware/vfabric-tc-server-standard/tcruntime-instance.sh" ) do
@@ -28,7 +27,6 @@ Puppet::Type.type(:tcruntime_instance).provide(:tcruntime_instance) do
     all_opts = " "
     if resource[:templates]
       template_opts = "-t " + resource[:templates].join(" -t ")
-      puts "Template Opts: " + template_opts
       all_opts = all_opts + template_opts
     end
     
@@ -37,28 +35,28 @@ Puppet::Type.type(:tcruntime_instance).provide(:tcruntime_instance) do
       resource[:properties].each do |k,v|
         property_opts = property_opts + " -p #{k}=#{v} "
       end
-      puts "Props: " + property_opts
       all_opts = all_opts + property_opts
     end
 
     if resource[:version]
       version_opt = "-v #{resource[:version]} "
-      puts "Version: " + version_opt
       all_opts = all_opts + version_opt
     end
 
     if resource[:layout]
       layout_opt = "--layout #{resource[:layout]} "
-      puts "Layout: " + layout_opt
       all_opts = all_opts + layout_opt
     end
 
     if resource[:properties_file]
       properties_file_opt = "-f #{resource[:properties_file]} "
-      puts "Prop file: " + properties_file_opt
       all_opts = all_opts + properties_file_opt
     end
-    puts all_opts
+
+    if resource[:use_java_home]
+      java_home_opt = "--java-home ${resource[:java_home]} "
+      all_opts = all_opts + java_home_opt
+    end
     tcruntime_instance('create', resource[:name], *all_opts.split(" "))
   end
 
