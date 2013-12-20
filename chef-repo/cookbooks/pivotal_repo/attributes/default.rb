50fb571ebebb2e4bbe519048da4190042ad21b47
Wes Schlichter <wschlichter@gopivotal.com>
2013-12-20 10:27:21 -0600

    Fixed issue with repo on RHEL

--- chef-repo/cookbooks/pivotal_repo/attributes/default.rb
+++ chef-repo/cookbooks/pivotal_repo/attributes/default.rb
@@ -1,19 +1,20 @@
 
 case node['platform']
 when 'redhat', 'centos'
-  if node['platform_version'].starts_with?('5')
+  if node['platform_version'].start_with?('5')
     rhel_version = 5
-  elsif node['platform_version'].starts_with?('6')
+  elsif node['platform_version'].start_with?('6')
     rhel_version = 6
   end
-  default['pivotal_repo']['package'] = "http://packages.gopivotal.com/pub/rpm/rhel#{rhel_version}/vfabric/5.3/vfabric-5.3-repo-5.3-1.noarch.rpm"
+  default['pivotal_repo']['package'] = "vfabric-5.3-repo-5.3-1.noarch.rpm"
+  default['pivotal_repo']['url'] = "http://packages.gopivotal.com/pub/rpm/rhel#{rhel_version}/vfabric/5.3/#{node['pivotal_repo']['package']}"
   default['pivotal_repo']['gpg_url'] = "http://packages.gopivotal.com/pub/rpm/rhel#{rhel_version}/vfabric/5.3/RPM-GPG-KEY-VFABRIC-5.3-EL#{rhel_version}"
   default['pivotal_repo']['eula_accept_cmd'] = "/etc/vmware/vfabric/vfabric-eula-acceptance.sh --accept_eula_file=VMware_EULA_20120515b_English.txt > /dev/null 2>&1"
   default['pivotal_repo']['eula_accepted_file'] = "/etc/vmware/vfabric/VMware_EULA_20120515b_English.txt"
 when 'ubuntu'
   if node['platform_version'] == '10.04'
     default['pivotal_repo']['package'] = "vfabric-repo-lucid_1.0-6_all.deb"
-  elsif node['platformv_ersion'] == '12.04'
+  elsif node['platform_version'] == '12.04'
     default['pivotal_repo']['package'] = "vfabric-repo-precise_1.0-6_all.deb"
   end
   default['pivotal_repo']['url'] = "http://packages.gopivotal.com/pub/apt/ubuntu/#{node['pivotal_repo']['package']}"
--- chef-repo/cookbooks/pivotal_repo/recipes/_rhel.rb
+++ chef-repo/cookbooks/pivotal_repo/recipes/_rhel.rb
@@ -14,12 +14,21 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 
-execute "import-gpgkey" do
-  command "rpm --import #{node['pivotal_repo']['gpg_key']}"
+execute "import-key" do
+  command "rpm --import #{node['pivotal_repo']['gpg_url']}"
+  action :nothing
 end
 
-execute "install-repo" do
-  command "rpm -Uvh #{node['pivotal_repo']['package']}"
+remote_file "#{Chef::Config[:file_cache_path]}/#{node['pivotal_repo']['package']}" do
+  source node['pivotal_repo']['url']
+  action :create_if_missing
+  notifies :run, "execute[import-key]", :immediately
+end
+
+rpm_package node['pivotal_repo']['package'] do
+  source "#{Chef::Config[:file_cache_path]}/#{node['pivotal_repo']['package']}"
+  only_if { ::File.exists?("#{Chef::Config[:file_cache_path]}/#{node['pivotal_repo']['package']}") }
+  action :install
 end
 
 execute "accept-eula" do
