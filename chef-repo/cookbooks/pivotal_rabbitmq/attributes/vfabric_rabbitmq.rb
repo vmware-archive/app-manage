908da04c5f01f027909bc6e320508982b40e885c
Wes Schlichter <wschlichter@gopivotal.com>
2014-01-05 10:53:49 -0600

    Initial commit of rabbitmq work.

---
+++ chef-repo/cookbooks/pivotal_rabbitmq/attributes/vfabric_rabbitmq.rb
@@ -0,0 +1,12 @@
+
+case node['platform_family']
+when 'debian'
+  default['rabbitmq']['use_distro_version'] = "true"
+when 'rhel'
+  if (node['platform_version'].start_with?("5"))
+    default['rabbitmq']['package'] = "http://packages.gopivotal.com/pub/rpm/rhel5/vfabric/5.3/x86_64/vfabric-rabbitmq-server-3.2.1-3.x86_64.rpm"
+  elsif (node['platform_version'].start_with?("6"))
+    default['rabbitmq']['package'] = "http://packages.gopivotal.com/pub/rpm/rhel6/vfabric/5.3/x86_64/vfabric-rabbitmq-server-3.2.1-3.x86_64.rpm"
+  end
+end
+
---
+++ chef-repo/cookbooks/pivotal_rabbitmq/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: pivotal_rabbitmq
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'rabbitmq::default'
+
