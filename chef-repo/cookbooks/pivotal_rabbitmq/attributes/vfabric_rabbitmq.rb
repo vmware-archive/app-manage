0c666b83a75403cc559c4ae2d8ba679d8d454571
Wes Schlichter <wschlichter@gopivotal.com>
2014-01-05 13:10:23 -0600

    Added a comment to the attributes to explain what is going on

--- chef-repo/cookbooks/pivotal_rabbitmq/attributes/vfabric_rabbitmq.rb
+++ chef-repo/cookbooks/pivotal_rabbitmq/attributes/vfabric_rabbitmq.rb
@@ -1,9 +1,12 @@
 
+
+# We need to override soem of the defaults
 node.override['rabbitmq']['version'] = "3.2.1"
 node.override['rabbitmq']['package-release'] = "3"
 
 case node['platform_family']
 when 'debian'
+  # For Ubuntu the Pivotal Package names are the same as the distro versions but on RHEL they have vfabric- in front of their names
   node.override['rabbitmq']['use_distro_version'] = "true"
 when 'rhel'
   if (node['platform_version'].start_with?("5"))
