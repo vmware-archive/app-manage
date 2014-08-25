ba6dd398c53bbc3bed9e5bcc5651ed43b6bcd01e
Wes Schlichter <wschlichter@pivotal.io>
2014-08-25 08:02:11 -0500

    Updated copyright headers to Pivotal Software, Inc

--- chef-repo/cookbooks/pivotal_tcserver/attributes/default.rb
+++ chef-repo/cookbooks/pivotal_tcserver/attributes/default.rb
@@ -1,4 +1,4 @@
-## Copyright 2014 GoPivotal, Inc
+## Copyright 2014 Pivotal Software, Inc
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
